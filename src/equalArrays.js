6437a956f7d71480923743125cfae74d70091d43
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-07 18:49:43 -0500

    passes tests

--- src/equalArrays.js
+++ src/equalArrays.js
@@ -1,6 +1,47 @@
 /**
  * return true if two arrays are equal, false otherwise
  */
-function isEqual(a1, a2) {}
+function isEqual(a1, a2) {
+  // guard clause - input arrays must be same length;
+  if (a1.length !== a2.length) return false;
+
+  let a1Map = new Map();
+  a1.forEach((item) => {
+    let count = a1Map.get(item);
+    if (!count >= 1) {
+      count = 1;
+    } else {
+      count++;
+    }
+    const entry = [item, count];
+    a1Map.set(...entry);
+  });
+
+  let a2Map = new Map();
+  a2.forEach((item) => {
+    let count = a2Map.get(item);
+    if (!count >= 1) {
+      count = 1;
+    } else {
+      count++;
+    }
+    const entry = [item, count];
+    a2Map.set(...entry);
+  });
+
+  // double check that Maps are the same size even though Arrays were already checked
+  if (a1Map.size !== a2Map.size) {
+    return false;
+  }
+
+  // Check if maps have any key/values that do not match
+  for (let [key, value] of a1Map.entries()) {
+    if (a2Map.get(key) !== value) {
+      return false;
+    }
+  }
+  // only returns if check above passes
+  return true;
+}
 
 module.exports = isEqual;
